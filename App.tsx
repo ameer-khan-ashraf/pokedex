5334cd0d7886a16d3f81cec4b10e7a4f2f076169
Ameer Khan <ameerkashraf@gmail.com>
2023-06-07 15:13:49 +0400

    first commit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
+
+const pokePath = "https://pokeapi.co/api/v2/"
+const pokeQuery = "pokemon?limit=151&offset=0"
 
 export default function App() {
+  const [firstGenPokemon, setFirstGenPokemon] = useState<any>([])
+  useEffect(() => {
+    const fetchPokemon = async () =>{
+      const response = await fetch(`${pokePath}${pokeQuery}`)
+      const data = await response.json() 
+    }
+    fetchPokemon()
+  }, [])
+
+  useEffect(() => {
+    console.log(firstGenPokemon)
+  
+  }, [firstGenPokemon])
+  
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>First Gen Pokemon</Text>
+      <ScrollView>
+        {firstGenPokemon.map((pokemon:any, index:any)=>{
+          return <Text key={index}>
+            {pokemon.name}
+          </Text>
+        })}
+      </ScrollView>
     </View>
   );
 }
